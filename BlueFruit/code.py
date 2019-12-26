cfb06267a7793f3cca53c9379cd7d42eea8a53b9
Darren Pruitt <darren.pruitt@gmail.com>
2019-12-26 10:20:15 -0600

    Added buttons and neopixels

--- BlueFruit/code.py
+++ BlueFruit/code.py
@@ -1,13 +1,20 @@
 import time
 import board
 import pulseio
+import neopixel
 import digitalio
 
+#if switch.value is True: # switch is slid to the left
 switch = digitalio.DigitalInOut(board.SLIDE_SWITCH)
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.UP
 
-#if switch.value is True: # switch is slid to the left
+# button down is True
+buttonA = digitalio.DigitalInOut(board.BUTTON_A)
+buttonA.switch_to_input(pull=digitalio.Pull.DOWN)
+
+buttonB = digitalio.DigitalInOut(board.BUTTON_B)
+buttonB.switch_to_input(pull=digitalio.Pull.DOWN)
 
 # Enable the speaker
 speaker_enable = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
@@ -16,13 +23,78 @@ speaker_enable.value = True
  
 # For the M0 boards:
 piezo = pulseio.PWMOut(board.SPEAKER, duty_cycle=0, frequency=440, variable_frequency=True)
- 
+
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.05, auto_write=False)
+
+led = digitalio.DigitalInOut(board.D13)
+led.switch_to_output()
+
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
+
+
+def color_chase(color, wait):
+    for i in range(10):
+        pixels[i] = color
+        time.sleep(wait)
+        pixels.show()
+    time.sleep(0.5)
+
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(10):
+            rc_index = (i * 256 // 10) + j * 5
+            pixels[i] = wheel(rc_index & 255)
+        pixels.show()
+        time.sleep(wait)
+
+
+def rainbow(wait):
+    for j in range(255):
+        for i in range(len(pixels)):
+            idx = int(i + j)
+            pixels[i] = wheel(idx & 255)
+        pixels.show()
+        time.sleep(wait)
+
+
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+WHITE = (255, 255, 255)
+OFF = (0, 0, 0)
+
 while True:
-    while switch.value:
+    if switch.value:
         for f in (262, 294, 330, 349, 392, 440, 494, 523):
             piezo.frequency = f
             piezo.duty_cycle = 65536 // 2  # On 50%
             time.sleep(0.25)  # On for 1/4 second
             piezo.duty_cycle = 0  # Off
             time.sleep(0.05)  # Pause between notes
-        time.sleep(0.5)
+        
+
+    if buttonA.value:  # button is pushed
+        led.value = True
+        rainbow_cycle(0.001)
+    else:
+        led.value = False
+        color_chase(OFF, 0.01)
+    
+    time.sleep(0.5)
